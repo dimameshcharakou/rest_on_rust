45dcf2b072b9058566f47978b15694a5ebaf3765
Dima Mescheryakov <dima.mescheryakov@gmail.com>
2015-10-17 14:06:36 +0200

    connection with postgres. Add ini file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,82 @@
+extern crate postgres;
+extern crate ini;
+
+use postgres::{Connection, ConnectParams, ConnectTarget, SslMode, UserInfo};
+use ini::Ini;
+use std::str::FromStr;
+
+fn params() -> (ConnectParams, SslMode) {
+    let conf = Ini::load_from_file(".phonebookrc").unwrap();
+    let general = conf.general_section();
+
+    let host = general.get("host").unwrap();
+    let port = general.get("port").unwrap();
+    let sslmode = general.get("sslmode").unwrap();
+    let dbname = general.get("dbname").unwrap();
+    let user = general.get("user").unwrap();
+    let pass = general.get("pass").unwrap();
+
+    let sslmode_ = match sslmode.as_ref() {
+        "disable" => SslMode::None,
+        "enable" => unimplemented!(),
+        _ => panic!("Wrong sslmode"),
+    };
+
+    let params = ConnectParams {
+        target: ConnectTarget::Tcp(host.to_owned()),
+        port: Some(FromStr::from_str(port).unwrap()),
+        user: Some(UserInfo {
+            user: user.to_owned(),
+            password: Some(pass.to_owned()),
+        }),
+        database: Some(dbname.to_owned()),
+        options: vec![],
+    };
+    (params, sslmode_)
+}
+
+struct Person {
+    id: i32,
+    name: String,
+    data: Option<Vec<u8>>
+}
+
 fn main() {
-    println!("Hello, world!");
+    let (params, sslmode) = params();
+    let conn =
+        Connection::connect(
+            "postgres://postgres:postgres@localhost",
+            &SslMode::None)
+        .unwrap();
+
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS person (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR NOT NULL,
+            data BYTEA
+        )",
+        &[])
+        .unwrap();
+
+    let me = Person {
+        id: 0,
+        name: "Dima".to_string(),
+        data: None
+    };
+
+    conn.execute(
+        "INSERT INTO person (name, data) VALUES ($1, $2)",
+        &[&me.name, &me.data])
+        .unwrap();
+
+    let stmt = conn.prepare("SELECT id, name, data FROM person").unwrap();
+
+    for row in stmt.query(&[]).unwrap() {
+        let person = Person {
+            id: row.get(0),
+            name: row.get(1),
+            data: row.get(2)
+        };
+        println!("Found person: {}", person.name);
+    }
 }
